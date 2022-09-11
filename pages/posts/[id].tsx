1a23341dc87ec883e6fe8faa39ff769907b88749
XUEHUANG521 <75666585+XUEHUANG521@users.noreply.github.com>
2022-09-11 20:15:14 +1000

    setup redux and material ui

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,20 @@
 import { useRouter } from "next/router";
 import { NextPage } from "next";
+import axios from "axios";
 
 const Post:NextPage = () => {
 	const router = useRouter();
 	const {id} = router.query;
+	const hello = () => {
+		axios.get('http://localhost:3000/api/hello').then(res =>
+			console.log(res)
+		)
+		
+	}
 	return (
 		<>
 		<div>Post: {id}</div>
+		<button onClick={hello}>Click me</button>
 		</>
 	)
 }
---
+++ store/index.tsx
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { ThunkAction,Action } from "@reduxjs/toolkit";
+
+const store = configureStore({
+	reducer: {}
+})
+
+export default store;
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+   ReturnType,
+   RootState,
+   unknown,
+   Action<string>
+ >;
